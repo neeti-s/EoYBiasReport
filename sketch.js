153516280ab1a00d67c7c5589964ce14392b6f46
Apoorva Avadhana <apoorvaavadhana@gmail.com>
2023-12-06 04:07:55 -0500

    init is created inside div so delete button works

--- sketch.js
+++ sketch.js
@@ -2,33 +2,13 @@ import { initializeFirebase, printUsernamePath } from './firebaseAuth.js';
 import { askForWords, generateAssumptions, generateQuestions, createInputBoxWithQuestion, requestWordsFromReplicate } from './QuestionsAssumptions.js';
 import { ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
-let dataBase;
-let username;
-
-fetch('firebaseConfig.json')
-    .then(response => response.json())
-    .then(data => {
-        const firebaseData = initializeFirebase(data);
-        const {
-            db,
-        } = firebaseData;
-        dataBase = db;  
-        printUsernamePath((updatedUsername) => {
-            username = updatedUsername;
-            console.log(username);
-        }); 
-   })
-   .catch(error => {
-    console.error('Error loading Firebase configuration:', error);
-});
+//  
 
 let projectFolder;
 let assumptionInDB; //create assumption folder in project folder
 let questionInDB; //create question folder in project folder
 // const textDiv = document.getElementById("resulting_text");
 // const waitingDiv = document.getElementById("waiting_text");
-
-init()
 
 //Project Name
 let projectTitle = document.getElementById("projectTitle");
@@ -66,10 +46,16 @@ formButton.addEventListener('click', () => {
 })
 projectTitle.appendChild(formButton);
 
+init()
+const addButton = document.getElementById("addButton"); // Get the button element
+addButton.addEventListener("click", init); // Add click event listener to the button
+
 
 function init() {
     console.log("init");    
     let text_container = document.getElementById("text_container");
+
+    const assumptionDiv = document.createElement("div");
     let input_field = document.createElement("input");
     input_field.type = "text";
     input_field.id = "input_prompt";
@@ -85,41 +71,43 @@ function init() {
         push(assumptionInDB, input_field.value); 
     });
 
-    let clearButton = document.createElement("button");
-    clearButton.textContent = "Clear";
-    clearButton.style.backgroundColor = "#593128";
-    clearButton.addEventListener("click", function () {
-        input_field.value = "";
-    });
+    // let clearButton = document.createElement("button");
+    // clearButton.textContent = "Clear";
+    // clearButton.style.backgroundColor = "#593128";
+    // clearButton.addEventListener("click", function () {
+    //     input_field.value = "";
+    // });
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
     deleteButton.style.backgroundColor = "#593128";
-    deleteButton.addEventListener('click', function() {
-        // text_container.remove(); 
+    deleteButton.addEventListener('click', function(e) {
+        e.target.parentElement.remove(); 
         //how to delete new assumption?
     })
 
-        let addButton = document.createElement("button");
-        addButton.textContent = "Add Assumption";
-        addButton.style.backgroundColor = "#1E1A26";
-        addButton.addEventListener("click", function () {
-            init();
-            text_container.append(deleteButton);
-        });
+    // let addButton = document.createElement("button");
+    // addButton.textContent = "Add Assumption";
+    // addButton.style.backgroundColor = "#1E1A26";
+    // addButton.addEventListener("click", function () {
+    //     init();
+    //     text_container.append(deleteButton);
+    // });
 
     
-        text_container.append(input_field);
-        text_container.append(submitButton);
-        text_container.append(clearButton);
-        text_container.append(addButton);
+    assumptionDiv.append(input_field);
+    assumptionDiv.append(submitButton);
+    assumptionDiv.append(deleteButton);
+    text_container.append(assumptionDiv);
+    // text_container.append(clearButton);
+    // text_container.append(addButton);
 
-        // input_field.addEventListener("keyup", function (event) {
-        //     if (event.key === "Enter") {
-        //         askForWords(input_field.value);
-        //         push(promptInDB, input_field.value);
-        //     }
-        // });
+    // input_field.addEventListener("keyup", function (event) {
+    //     if (event.key === "Enter") {
+    //         askForWords(input_field.value);
+    //         push(promptInDB, input_field.value);
+    //     }
+    // });
 }
 
 function printAssumptionInDB() {
