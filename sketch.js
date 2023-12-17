8f12192bdf2ca5e60601f9c772866239e5d9becf
neeti-s <123490539+neeti-s@users.noreply.github.com>
2023-12-17 13:50:18 -0500

    Print assumption

--- sketch.js
+++ sketch.js
@@ -67,6 +67,12 @@ function init() {
         mainassumptioncounter = true;
         document.getElementById("current_question").innerHTML = "Write a new assumption";
     })
+
+    const printButton = document.getElementById("print_button");
+    // addButton.addEventListener("click", init); // Add click event listener to the button
+    printButton.addEventListener("click", function(e) {
+        window.print();
+    })
 }
 
 function getLastSavedAssumption() {
